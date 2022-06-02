6f82fca3588b268629f200aae34c6e7b2bbcf712
BIG-DEVON <ezeajie14@gmail.com>
2022-06-02 10:57:41 +0100

    ..

--- config/conn.php
+++ config/conn.php
@@ -1,11 +1,11 @@
 <?php
 
+//development connection
 
+//$conn= mysqli_connect("localhost","root","","lms");
 
-
-
- 
- $conn= mysqli_connect("localhost","root","","lms");
+ //remote conn
+ $conn= mysqli_connect("us-cdbr-east-05.cleardb.net","bdc7f0a905d4bf","5dea9c14 ","heroku_9657b2c173deb38");
  if(!$conn){
      echo "Connection Error".mysqli_connect_error( );
  }
